54d5d3df8e3404ebed9c347d7bb7ba0b2732d6ed
nandasp23805 <nandasatriaperwira23805@gmail.com>
2024-10-26 21:02:59 +0700

    Add files via upload

---
+++ BMIberaksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BMI;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BMIberaksi {
+    
+    public static void main(String[] args) {
+        System.out.println("selamat datang di BMI");
+        System.out.println("hasil anda akan keluar dibawah");
+        System.out.println("-------------------------------");
+
+        BMI BMIKU = new BMI(151 , 42);
+        BMIKU.getBmi();
+    }
+    
+}
